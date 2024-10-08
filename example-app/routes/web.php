d176edc8f67ad14ac0c536e29ba62192ba2ab916
Osicasta <oscar.castano@upb.edu.co>
2024-10-08 01:19:37 -0500

    avance pagina web hamburguesa

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::resource('tasks', TaskController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
